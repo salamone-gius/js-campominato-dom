81c6bf873ccee5dcd0fc2ad98bd674f0bbf41b15
salamone-gius <salamone.gius@gmail.com>
2022-05-10 18:36:51 +0200

    difficult setting added (bonus)

--- main.js
+++ main.js
@@ -14,13 +14,61 @@
 // con difficoltà 2 => tra 1 e 50
 
 
-let userTries = 3;
+
+// GAME SETTINGS \\
+const numberOfBombs = 16;
+let level;
+
+let bombs = [];
+let userNumbers = [];
+let minRange = 1;
+let maxRange;
+
+console.log(`${"level ="} ${level}`);
+console.log(`${"maxRange ="} ${maxRange}`);
+
+
+// BONUS: (da fare solo se funziona tutto il resto)
+// all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
+alert("Benvenuto a Campo Minato.")
+alert("Inserisci un livello di difficoltà.");
+// BONUS.1 FINTANTO CHE l'utente inserisce un NaN OPPURE un numero minore di 0 OPPURE maggiore di 2
+while (isNaN(level) || (level < 0) || (level > 2)) {
+    // BONUS.2 continuo a chiedere all'utente di inserire un livello di difficoltà
+    level = Number(prompt("0=facile  1=medio  2=difficile"));
+    // BONUS.3 SE l'utente inserisce un NaN
+    if (isNaN(level) || (level < 0) || (level > 2)) {
+        // BOUNS.4 glielo faccio notare
+        alert("Devi inserire un NUMERO tra 0, 1 e 2.");
+    // BONUS.5 ALTRIMENTI niente
+    } else {
+    }
+}
+
+// BONUS.6 SE il numero inserito dall'utente (level) è 0
+if (level === 0) {
+    // BOUNS.7 setto il range massimo dei numeri casuali generati a 100
+    maxRange = 100;
+// BONUS.8 ALTRIMENTI SE il numero inserito dall'utente (level) è 1
+} else if (level === 1) {
+    // BOUNS.9 setto il range massimo dei numeri casuali generati a 80
+    maxRange = 80;
+// BONUS.10 ALTRIMENTI (il numero inserito dall'utente (level) è 2)
+} else {
+    // BOUNS.11 setto il range massimo dei numeri casuali generati a 50
+    maxRange = 50;
+}
+
+const userTries = maxRange - numberOfBombs;
+
+
+console.log(`${"level ="} ${level}`);
+console.log(`${"maxRange ="} ${maxRange}`);
+console.log(`${"userTries ="} ${userTries}`);
 
 // 1. faccio generare al computer 16 numeri casuali tra 1 e 100 (non ci possono essere duplicati);
-// 1.1 creo un array vuoto che popolerò con 16 numeri casuali
-let bombs = [];
 // 1.2 FINTANTO CHE l'array bombs contiene meno di 16 elementi
-while (bombs.length < 16) {
+while (bombs.length < numberOfBombs) {
     // 1.3 creo un numero casuale
     let randomNumber = Math.floor(Math.random() * 100) + 1;
     // 1.4 Scorrendo l'array devo controllare che il numero casuale non sia presente nell'array bombs
@@ -35,41 +83,35 @@ while (bombs.length < 16) {
     }
 }
 
-console.log(`${"Array bombs ="} ${bombs}`);
-
 // 2. chiedo all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
 //    L’utente non può inserire più volte lo stesso numero.
-// 2.1 creo un array vuoto che popolerò con i numeri inseriti dall'utente
-let userNumbers = [];
-// 2.2 imposto una variabile con valore 'false' PER CONTROLLARE I CICLI
+// 2.1 imposto una variabile con valore 'false' PER CONTROLLARE I CICLI
 let gameOver = false;
-// 2.3 per x volte: chiedo all'utente di inserire un numero
-// 2.4 FINTANTO CHE la lunghezza dell'array userNumbers (numeri scelti dall'utente) è minore del numero di tentativi a disposizione E gameOver è settato su 'false'
+// 2.2 per x volte: chiedo all'utente di inserire un numero
+// 2.3 FINTANTO CHE la lunghezza dell'array userNumbers (numeri scelti dall'utente) è minore del numero di tentativi a disposizione E gameOver è settato su 'false'
 while ((userNumbers.length < userTries) && (gameOver === false)) {
-    // 2.5 dico all'utente di inserire un numero tra 1 e 100
-    let userNumb = Number(prompt("Inserisci un numero tra 1 e 100"));
-    // 2.6 SE l'utente inserisce un numero già presente nell'array bombs
+    // 2.4 dico all'utente di inserire un numero tra 1 e 100
+    let userNumb = Number(prompt("Inserisci un numero tra 1 e 100."));
+    // 2.5 SE l'utente inserisce un numero già presente nell'array bombs
     if (bombs.includes(userNumb)) {
-        // 2.7 la partita termina e l'utente ha perso
+        // 2.6 la partita termina e l'utente ha perso
         alert("Hai beccato una bomba!");
         gameOver = true;
-    // 2.8 ALTRIMENTI SE l'utente inserisce un NaN OPPURE un numero minore o uguale a 0 OPPURE maggiore di 100
+    // 2.7 ALTRIMENTI SE l'utente inserisce un NaN OPPURE un numero minore o uguale a 0 OPPURE maggiore di 100
     } else if (isNaN(userNumb) || (userNumb <= 0) || (userNumb > 100)) {
-        // 2.9 gli dico dolcemente che ha sbagliato
+        // 2.8 gli dico dolcemente che ha sbagliato
         alert("Sai leggere? Ho detto un NUMERO TRA 1 E 100!");
-    // 2.10 ALTRIMENTI SE l'utente inserisce un numero che aveva già inserito precedentemente
+    // 2.9 ALTRIMENTI SE l'utente inserisce un numero che aveva già inserito precedentemente
     } else if (userNumbers.includes(userNumb)) {
-        // 2.11 lo avverto che lo aveva già inserito
+        // 2.10 lo avverto che lo aveva già inserito
         alert("Hai già inserito questo numero.")
-    // 2.12 ALTRIMENTI
+    // 2.11 ALTRIMENTI
     } else {
-        // 2.13 tranquillizzo l'utente, aggiungo il suo numermo all'array userNumbers e continuo
+        // 2.12 tranquillizzo l'utente, aggiungo il suo numermo all'array userNumbers e continuo
         alert("Ottimo! Fin qui niente bombe. Continua.")
         userNumbers.push(userNumb);
     }
 }
-
-console.log(gameOver);
 
 // 3. Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 // 3.1 SE l'utente ha beccato una bomba (gameOver === true)
@@ -84,6 +126,3 @@ if (gameOver === true) {
 
 // 3.5 comunico (in ogni caso) all'utente il suo punteggio (di quanti elementi è composto l'array dei suoi numeri)
 alert(`${"Il tuo punteggio:"} ${userNumbers.length}`);
-
-
-console.log(`${"Array userNumbers ="} ${userNumbers}`);
